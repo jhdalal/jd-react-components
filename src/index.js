3a5f650a8a81f6586bcc92f34eb4d24ccad918c0
Jignesh Dalal <jignesh.h.dalal@gmail.com>
2018-06-05 22:07:16 +0530

    Boilerplate for reusable components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Docs from './docs/Docs';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import '../node_modules/highlight.js/styles/vs2015.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Docs />, 
+    document.getElementById('root')
+);
